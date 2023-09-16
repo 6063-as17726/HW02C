295c63fe0c36fe2d0ac0f48d97e05177efcc937d
Ayushi Shah <as17726@nyu.edu>
2023-09-16 18:05:34 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,18 @@
 function setup() {
+    createCanvas(windowWidth, windowHeight); 
+    background (150,150,150);
 }
 
 function draw() {
+fill (0);
+    rect(0,0,600,500);
+    strokeWeight(5);
+    stroke(255); 
+    ellipse(50, 100, 50, 50);
+    ellipse(75,160,80,50);
+    ellipse(125,200,20,70);
+    angleMode(degrees);
+    rotate(100);
+    ellipse(60,300,80,100);
+    ellipse(30,400,80,200);
 }
